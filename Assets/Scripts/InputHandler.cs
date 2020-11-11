b51a83a1f8b65932122262576cf56bf77517a37a
Feverfew826 <lightkill826@gmail.com>
2020-11-11 11:49:42 +0900

    Jum And Reach Game
    Hmm...

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputHandler : MonoBehaviour
+{
+    public interface PermissionProvider
+    {
+        bool HasPermission();
+    }
+    private PermissionProvider permission;
+
+    public interface InputReceiver
+    {
+        void OnHorizontalAxis(float horizontalAxis);
+        void OnVerticalAxis(float verticalAxis);
+        void OnJumpButtonDown();
+    }
+
+    private HashSet<InputReceiver> inputReceivers;
+    private HashSet<InputReceiver> inputReceiverRegisterAwaiters;
+    private HashSet<InputReceiver> inputReceiverUnregisterAwaiters;
+
+    private void Awake()
+    {
+        inputReceivers = new HashSet<InputReceiver>();
+        inputReceiverRegisterAwaiters = new HashSet<InputReceiver>();
+        inputReceiverUnregisterAwaiters = new HashSet<InputReceiver>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputReceivers.UnionWith(inputReceiverRegisterAwaiters);
+        inputReceiverRegisterAwaiters.Clear();
+        inputReceivers.ExceptWith(inputReceiverUnregisterAwaiters);
+        inputReceiverUnregisterAwaiters.Clear();
+
+        if(permission != null && permission.HasPermission())
+        {
+            foreach(var receiver in inputReceivers)
+            {
+                receiver.OnHorizontalAxis(Input.GetAxis("Horizontal"));
+                receiver.OnVerticalAxis(Input.GetAxis("Vertical"));
+                
+                if (Input.GetButtonDown("Jump"))
+                    receiver.OnJumpButtonDown();
+            }
+        }
+    }
+
+    public void SetPermissionProvider(PermissionProvider newPermissionProvider)
+    {
+        this.permission = newPermissionProvider;
+    }
+
+    public void AddInputReceiverRegisterAwaiter(InputReceiver inputReceiver)
+    {
+        inputReceiverRegisterAwaiters.Add(inputReceiver);
+    }
+
+    public void AddInputReceiverUnregisterAwaiter(InputReceiver inputReceiver)
+    {
+        inputReceiverUnregisterAwaiters.Add(inputReceiver);
+    }
+
+}
---
+++ Assets/Scripts/MyGameMode.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MyGameMode : MonoBehaviour
+{
+    public InputHandler playerInputHandler;
+    public MyPlayer player;
+    public TriggerVolume[] deathTriggerVolumes;
+    public TriggerVolume winTriggerVolume;
+    public GameObject startPosition;
+
+    private class InputPermission : InputHandler.PermissionProvider
+    {
+        public bool enable;
+        public bool HasPermission()
+        {
+            return enable;
+        }
+    }
+    InputPermission playerInputPermission;
+
+    private class PlayerDeathHandler : MyPlayer.DeathHandler
+    {
+        MyGameMode myGameMode;
+        public PlayerDeathHandler(MyGameMode myGameMode)
+        {
+            this.myGameMode = myGameMode;
+        }
+        public void OnDeath()
+        {
+            myGameMode.StartCoroutine(myGameMode.GameOverAndRespawnCoroutine());
+        }
+    }
+    PlayerDeathHandler playerDeathHandler;
+
+    public class DeathTriggerVolumeHandler : TriggerVolume.Handler
+    {
+        public void OnEnter(TriggerVolume trigger, Collider2D collision)
+        {
+            MyPlayer myPlayer = collision.GetComponent<MyPlayer>();
+            if ( myPlayer != null)
+                myPlayer.Die();
+        }
+    }
+    DeathTriggerVolumeHandler deathTriggerVolumeHandler;
+
+    public class WinTriggerVolumeHandler : TriggerVolume.Handler
+    {
+        MyGameMode myGameMode;
+        public WinTriggerVolumeHandler(MyGameMode myGameMode)
+        {
+            this.myGameMode = myGameMode;
+        }
+        public void OnEnter(TriggerVolume trigger, Collider2D collision)
+        {
+            MyPlayer myPlayer = collision.GetComponent<MyPlayer>();
+            if (myPlayer != null)
+            {
+                myPlayer.Ceremony();
+                myGameMode.StartCoroutine(myGameMode.GameWinCoroutine());
+            }
+        }
+    }
+    WinTriggerVolumeHandler winTriggerVolumeHandler;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerInputPermission = new InputPermission();
+        playerInputPermission.enable = true;
+        playerInputHandler.SetPermissionProvider(playerInputPermission);
+
+        playerDeathHandler = new PlayerDeathHandler(this);
+        player.deathHandler = playerDeathHandler;
+
+        deathTriggerVolumeHandler = new DeathTriggerVolumeHandler();
+        foreach (var volume in deathTriggerVolumes)
+            volume.handler = deathTriggerVolumeHandler;
+
+        winTriggerVolumeHandler = new WinTriggerVolumeHandler(this);
+        winTriggerVolume.handler = winTriggerVolumeHandler;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator GameOverAndRespawnCoroutine()
+    {
+        playerInputPermission.enable = false;
+        Debug.Log("GameOver");
+        yield return new WaitForSeconds(3);
+        player.TeleportAt( startPosition.transform.position );
+        playerInputPermission.enable = true;
+    }
+
+    IEnumerator GameWinCoroutine()
+    {
+        playerInputPermission.enable = false;
+        Debug.Log("GameWin");
+        yield return new WaitForSeconds(3);
+        MyGameInstance.instance.hasCleared = true;
+        SceneManager.LoadScene(0);
+    }
+
+}
